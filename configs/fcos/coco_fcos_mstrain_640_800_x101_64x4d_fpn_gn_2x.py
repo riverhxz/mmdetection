ab10b8d715db96f1a0a10b328db4e300a868b832
riverhxz <ravehun@qq.com>
2019-07-10 00:57:56 +0800

    add cascade mast

--- configs/fcos/coco_fcos_mstrain_640_800_x101_64x4d_fpn_gn_2x.py
+++ configs/fcos/coco_fcos_mstrain_640_800_x101_64x4d_fpn_gn_2x.py
@@ -104,7 +104,7 @@ optimizer = dict(
     momentum=0.9,
     weight_decay=0.0001,
     paramwise_options=dict(bias_lr_mult=2., bias_decay_mult=0.))
-optimizer_config = dict(grad_clip=None)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
     policy='step',
